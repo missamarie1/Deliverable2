749b7a390fdbe5fce47a60089855bad02b46509a
marissa Jacklyn <missamarie0217@gmail.com>
2022-03-28 21:44:51 -0400

    update with console logs

--- script.js/script.js
+++ script.js/script.js
@@ -6,6 +6,7 @@
 
 let name = prompt("Welcome to GC mini-golf! What is your name?");
 
+
 let holes = prompt("Hi, "+name+"! Would you like to play 3 or 6 holes?");
 let totalPuts = 0;
 
@@ -14,33 +15,29 @@ if (holes == 3 || holes == 6) {
     for (let i = 1; i <= holes; i++) {
         let numOfPuts = prompt("How many putts for hole "+i+"?");
         totalPuts = totalPuts + parseInt(numOfPuts);
-       
     }
+    console.log(name);
     alert(totalPuts);
 }
 
-if (holes == 3, totalPuts > 9) {
-    alert("Nice try, "+name+"! Your total par was: +"+totalPuts+"");
+if (holes == 3 && totalPuts > 9) {
+    console.log("Nice try "+name+"! Your total par was: +"+totalPuts+"");
 }
 
-if (holes == 3, totalPuts < 9) {
-    alert("Great job, "+name+"! Your total par was: -"+totalPuts+"");
-
+else if(holes == 3 && totalPuts < 9){
+    console.log("Great job, "+name+". Your total par was: -"+totalPuts+"")
 }
 
-if (holes ==3, totalPuts == 9) {
-    alert("Good game, "+name+". Your total par was: 0");
+ else if(holes == 3 && totalPuts == 9){
+        console.log("Good game, "+name+". Your total par was: 0");
 }
 
-if (holes == 6, totalPuts > 18) {
-    alert("Nice try, "+name+"! Your total par was: +"+totalPuts+"");
+if (holes == 6 && totalPuts > 18) {
+   console.log("Nice try "+name+"! Your total par was: +"+totalPuts+"");}
+
+else if (holes == 6 && totalPuts < 18) {
+    console.log("Great job "+name+"! Your total par was: -"+totalPuts+"");
 }
 
-if (holes == 6, totalPuts < 18) {
-    alert("Great job, "+name+"! Your total par was: -"+totalPuts+"");
-
-}
-
-if (holes ==6, totalPuts == 18) {
-    alert("Good game, "+name+". Your total par was: 0");
-}
+else if (holes == 6 && totalPuts == 18) {
+    console.log("Good game "+name+". Your total par was: 0"); }
